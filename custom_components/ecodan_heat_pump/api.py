c5ed58cd12c72b21ae50f363c959e894ca532367
Michael Marconi <m@marconis.io>
2024-02-12 16:53:03 +0000

    WIP onboarding flow

--- custom_components/ecodan_heat_pump/api.py
+++ custom_components/ecodan_heat_pump/api.py
@@ -8,23 +8,23 @@ import aiohttp
 import async_timeout
 
 
-class IntegrationBlueprintApiClientError(Exception):
+class ApiClientError(Exception):
     """Exception to indicate a general API error."""
 
 
-class IntegrationBlueprintApiClientCommunicationError(
-    IntegrationBlueprintApiClientError
+class ApiClientCommunicationError(
+    ApiClientError
 ):
     """Exception to indicate a communication error."""
 
 
-class IntegrationBlueprintApiClientAuthenticationError(
-    IntegrationBlueprintApiClientError
+class ApiClientAuthenticationError(
+    ApiClientError
 ):
     """Exception to indicate an authentication error."""
 
 
-class IntegrationBlueprintApiClient:
+class ApiClient:
     """Sample API Client."""
 
     def __init__(
@@ -70,21 +70,21 @@ class IntegrationBlueprintApiClient:
                     json=data,
                 )
                 if response.status in (401, 403):
-                    raise IntegrationBlueprintApiClientAuthenticationError(
+                    raise ApiClientAuthenticationError(
                         "Invalid credentials",
                     )
                 response.raise_for_status()
                 return await response.json()
 
         except asyncio.TimeoutError as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise ApiClientCommunicationError(
                 "Timeout error fetching information",
             ) from exception
         except (aiohttp.ClientError, socket.gaierror) as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise ApiClientCommunicationError(
                 "Error fetching information",
             ) from exception
         except Exception as exception:  # pylint: disable=broad-except
-            raise IntegrationBlueprintApiClientError(
+            raise ApiClientError(
                 "Something really wrong happened!"
             ) from exception
--- custom_components/ecodan_heat_pump/config_flow.py
+++ custom_components/ecodan_heat_pump/config_flow.py
@@ -1,23 +1,36 @@
 """Adds config flow for Blueprint."""
+
 from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.helpers import selector
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_ACCESS_TOKEN
+from homeassistant.helpers.selector import (
+    TextSelector,
+    TextSelectorConfig,
+    TextSelectorType,
+)
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 
 from .api import (
-    IntegrationBlueprintApiClient,
-    IntegrationBlueprintApiClientAuthenticationError,
-    IntegrationBlueprintApiClientCommunicationError,
-    IntegrationBlueprintApiClientError,
+    ApiClient,
+    ApiClientAuthenticationError,
+    ApiClientCommunicationError,
+    ApiClientError,
 )
 from .const import DOMAIN, LOGGER
 
+USERNAME_1 = "username_1"
+USERNAME_2 = "username_2"
+USERNAME_3 = "username_3"
 
-class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Blueprint."""
+PASSWORD_1 = "password_1"
+PASSWORD_2 = "password_2"
+PASSWORD_3 = "password_3"
+
+
+class ConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for integration"""
 
     VERSION = 1
 
@@ -28,18 +41,19 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by the user."""
         _errors = {}
         if user_input is not None:
+            LOGGER.debug("Testing user input...")
             try:
                 await self._test_credentials(
                     username=user_input[CONF_USERNAME],
                     password=user_input[CONF_PASSWORD],
                 )
-            except IntegrationBlueprintApiClientAuthenticationError as exception:
+            except ApiClientAuthenticationError as exception:
                 LOGGER.warning(exception)
                 _errors["base"] = "auth"
-            except IntegrationBlueprintApiClientCommunicationError as exception:
+            except ApiClientCommunicationError as exception:
                 LOGGER.error(exception)
                 _errors["base"] = "connection"
-            except IntegrationBlueprintApiClientError as exception:
+            except ApiClientError as exception:
                 LOGGER.exception(exception)
                 _errors["base"] = "unknown"
             else:
@@ -49,30 +63,71 @@ class BlueprintFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                 )
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_USERNAME,
-                        default=(user_input or {}).get(CONF_USERNAME),
-                    ): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.TEXT
-                        ),
-                    ),
-                    vol.Required(CONF_PASSWORD): selector.TextSelector(
-                        selector.TextSelectorConfig(
-                            type=selector.TextSelectorType.PASSWORD
-                        ),
-                    ),
-                }
-            ),
+            step_id="init",
+            data_schema={
+                vol.Required(USERNAME_1): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="username"
+                    )
+                ),
+                vol.Required(PASSWORD_1): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="password"
+                    )
+                ),
+                vol.Optional(USERNAME_2): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="username"
+                    )
+                ),
+                vol.Optional(PASSWORD_2): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="password"
+                    )
+                ),
+                vol.Optional(USERNAME_3): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="username"
+                    )
+                ),
+                vol.Optional(PASSWORD_3): TextSelector(
+                    TextSelectorConfig(
+                        type=TextSelectorType.TEXT, autocomplete="password"
+                    )
+                ),
+            },
+            # data_schema=vol.Schema(
+            #     {
+            #         vol.Required(
+            #             CONF_USERNAME,
+            #             default=(user_input or {}).get(CONF_USERNAME),
+            #         ): selector.TextSelector(
+            #             selector.TextSelectorConfig(
+            #                 type=selector.TextSelectorType.TEXT
+            #             ),
+            #         ),
+            #         vol.Required(CONF_PASSWORD): selector.TextSelector(
+            #             selector.TextSelectorConfig(
+            #                 type=selector.TextSelectorType.PASSWORD
+            #             ),
+            #         ),
+            #         vol.Required(
+            #             CONF_ACCESS_TOKEN,
+            #             default=(user_input or {}).get(CONF_ACCESS_TOKEN),
+            #         ): selector.TextSelector(
+            #             selector.TextSelectorConfig(
+            #                 type=selector.TextSelectorType.TEXT
+            #             ),
+            #         ),
+            #     }
+            # ),
             errors=_errors,
         )
 
     async def _test_credentials(self, username: str, password: str) -> None:
         """Validate credentials."""
-        client = IntegrationBlueprintApiClient(
+        LOGGER.debug("Testing credentials...")
+        client = ApiClient(
             username=username,
             password=password,
             session=async_create_clientsession(self.hass),
